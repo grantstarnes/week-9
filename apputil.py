4918326915d7685f64823e84ffa416c0904021b2
Grant Starnes <gstarnes@iu.edu>
2025-10-28 18:33:35 -0400

    made sure X and y are being combined into the same DataFrame, grouped the DataFrame by the X columns, and included an if/else statement to calculate the mean or median based on the estimate

--- apputil.py
+++ apputil.py
@@ -14,6 +14,18 @@ class GroupEstimate(object):
             raise ValueError("X and y should have the same number of rows")
         if pd.isnull(y).any():
             raise ValueError("y must not contain missing values")
+        
+        df = X.copy()
+        df["__target__"] = y
+
+        if self.estimate == 'mean':
+            group_estimates = df.groupby(list(X.columns))["__target__"].mean().reset_index()
+        else:
+            group_estimates = df.groupby(list(X.columns))["__target__"].median().reset_index()
+
+        group_estimates.rename(columns = {"__target__": "estimate"}, inplace=True)
+
+        self.group_estimates_ = group_estimates
 
     def predict(self, X):
         return None
